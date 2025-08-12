41bdc7bf0d14f7dc4783979a5d49237867f4e195
aphear <afxefx932@gmail.com>
2025-08-12 22:06:12 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python -m pip install --upgrade pip==22.3.1
-pip install --no-cache-dir --ignore-installed -r requirements.txt
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
